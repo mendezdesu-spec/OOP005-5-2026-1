b36dddff21b48fa9c005fab45678b4d3060a3e39
mendezdesu-spec <mendezdesu@gmail.com>
2026-02-09 13:41:40 -0500

    Add files via upload

---
+++ Corte1/practica2/Proyecto2.java
@@ -0,0 +1,11 @@
+package proyecto2;
+
+public class Proyecto2 {
+    public int x, y, z;
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
