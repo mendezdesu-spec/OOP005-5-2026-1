44f1386c50520e54143b42a97d369288c13bb39b
mendezdesu-spec <mendezdesu@gmail.com>
2026-02-09 13:40:12 -0500

    Add files via upload

---
+++ Corte1/Practica1/HelloWorld.java
@@ -0,0 +1,10 @@
+package helloworld;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Helo world");
+        
+// TODO code application logic here
+    }    
+}
