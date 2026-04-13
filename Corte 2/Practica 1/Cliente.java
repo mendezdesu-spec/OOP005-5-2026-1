ad93d87ed73b4a13054847a4ab4bf4f20b1fadc4
mendezdesu-spec <mendezdesu@gmail.com>
2026-04-13 06:43:28 -0500

    Create Cliente.java

---
+++ Corte 2/Practica 1/Cliente.java
@@ -0,0 +1,55 @@
+package inheritance;
+
+import java.util.Date;
+
+public class Cliente extends Person {
+    
+    private int idCliente;
+    private Date fechaRegistro;
+    private boolean vip;
+    private static int contadorCliente;
+
+    public Cliente(Date fechaRegistro, boolean vip, String nombre, 
+                   char genero, int edad, String direccion) {
+        
+        super(nombre, genero, edad, direccion);
+        this.idCliente = ++Cliente.contadorCliente;
+        this.fechaRegistro = fechaRegistro;
+        this.vip = vip;
+    }
+
+    @Override
+    public String obtenerDetalles() {
+        return super.obtenerDetalles() + ", Es VIP: " + this.vip;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public Date getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(Date fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public boolean isVip() {
+        return vip;
+    }
+
+    public void setVip(boolean vip) {
+        this.vip = vip;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "idCliente=" + idCliente +
+                ", fechaRegistro=" + fechaRegistro +
+                ", vip=" + vip +
+                ", " + super.toString() +
+                '}';
+    }
+}
